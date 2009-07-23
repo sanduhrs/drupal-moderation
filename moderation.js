e769d5b781a258a7b32c40978c25b81541022a20
Stefan Auditor <stefan.auditor@erdfisch.de>
2009-07-23 11:23:10 +0000

    #518604 by binford2k: Link actions use backwards text

--- moderation.js
+++ moderation.js
@@ -42,10 +42,10 @@ Drupal.moderationButton = function() {
   var obj_id = id.split('-')[3];
   var url = window.location.protocol+'//'+window.location.hostname+$(this).attr("href");
   var text = {
-    'status': {0: 'not published', 1: 'published'},
-    'promote': {0: 'not promoted', 1: 'promoted'},
-    'sticky': {0: 'not sticky', 1: 'sticky'},
-    'moderate': {0: 'not moderated', 1: 'moderated'}
+    'status': {0: 'Publish', 1: 'Unpublish'},
+    'promote': {0: 'Promote', 1: 'Demote'},
+    'sticky': {0: 'Make sticky', 1: 'Remove stickiness'},
+    'moderate': {0: 'Moderate', 1: 'Unmoderate'}
   };
   
   $(this).click(function () {
