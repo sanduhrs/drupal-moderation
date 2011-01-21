78519859f90598e1ff78199c2be061789a2670fe
Stefan Auditor <stefan.auditor@erdfisch.de>
2011-01-21 15:10:14 +0100

    updating

--- sites/default/modules/contrib/moderation/README.txt
+++ sites/default/modules/contrib/moderation/README.txt
@@ -8,15 +8,19 @@ DESCRIPTION
 The moderation module provides a node and a comment queue.
 
 It allows moderators to review all posted content while already published.
-Included Ajax based functionality allows the moderators to preview the content and to change status like published, moderated, promoted, sticky without having to leave the page. A seperate page provides a log, listing all status changes with who and when it was changed.
+Included Ajax based functionality allows the moderators to preview the content
+and to change status like published, moderated, promoted, sticky without having
+to leave the page. A seperate page provides a log, listing all status changes
+with who and when it was changed.
 
-This project was developed by erdfisch (http://erdfisch.de) for ZEIT Online (http://www.zeit.de).
+This project was developed by erdfisch (http://erdfisch.de)
+for ZEIT Online (http://www.zeit.de).
 
 
 REQUIREMENTS
 ------------
 Drupal 6.x
-
+Views
 
 INSTALLING
 ----------
@@ -28,11 +32,19 @@ Read more about installing modules at http://drupal.org/node/70151
 
 CONFIGURING AND USING
 ---------------------
-1. Go to admin/settings/moderation. Set nodetypes that you want to be moderated. Click on 'Save configuration' button. 
+1. Go to admin/settings/moderation. Set nodetypes that you want to be moderated.
+   Click on 'Save configuration' button. 
 
-2. To start moderating CONTENT go to admin/content/node. Click on 'Moderation' tab. Click one of the following links beside each content:  Moderate, Make sticky, Demote, Unpublish, Edit. To clarify the location of those links view the following screenshot at http://drupal.org/node/442728
+2. To start moderating CONTENT go to admin/content/node.
+   Click on 'Moderation' tab. Click one of the following links beside each
+   content:  Moderate, Make sticky, Demote, Unpublish, Edit.
+   To clarify the location of those links view the following screenshot at http://drupal.org/node/442728
 
-3. To start moderating COMMENTS go to admin/content/comment. Click on 'Moderation' tab. Click one of the following links beside each comment:  Publish, moderate, edit.
+3. To start moderating COMMENTS go to admin/content/comment.
+   Click on 'Moderation' tab. Click one of the following links beside each
+   comment:  Publish, moderate, edit.
+
+4. A THREADED moderation queue is available too, go to admin/content/moderation.
 
 
 REPORTING ISSUE. REQUESTING SUPPORT. REQUESTING NEW FEATURE
--- sites/default/modules/contrib/moderation/moderation-views.js
+++ sites/default/modules/contrib/moderation/moderation-views.js
@@ -3,101 +3,114 @@
  *    Figure out how to reuse the moderation js.
  */
 (function($) {
-  Drupal.moderationPreview = function(context) {
-  $("a.moderation-ajax-preview:not('.moderation-preview-processed')", context)
-  .after('<div class="moderation-preview"></div>')
-  .click(function() {
-    var link = $(this);
-    var obj_id = $(this).siblings('div.moderation-preview-attributes').attr('entity-id');
-    var type = $(this).siblings('div.moderation-preview-attributes').attr('type');
-
-    var preview = $(this).siblings('div.moderation-preview');
-    var url = window.location.protocol+'//'+window.location.hostname+(window.location.port ? ':'+window.location.port : '')+Drupal.settings.basePath+"?q=moderation/" + type + "/" + obj_id + "/get/preview";
-
-    if (preview.html().length == 0) {
-      link.addClass('throbbing');
-
-      // load preview data
-      var html = $.ajax({
-        url: url+'&js=1',
-        dataType: "json",
-        success: function(html) {
-          preview.html(html);
-          Drupal.attachBehaviors(preview);
-          link.removeClass('throbbing');
-          preview.show();
-        }
-      });
-    }
-
-    preview.toggle();
-
-    $.ajax({
-      url: window.location.protocol+'//'+window.location.hostname+(window.location.port ? ':'+window.location.port : '')+Drupal.settings.basePath+"?q=moderation/" + type + "/"+obj_id+"/get/moderate&js=1",
-      dataType: "json",
-      success: function(status) {
-        preview.find('.moderation-messages').remove();
-        if (status == 1) {
-          preview.prepend('<div class="moderation-messages status">'+Drupal.t('This item has been moderated in the meantime.')+'</div>');
-        }
-      }
-    });
-
-    return false;
-  })
-  .addClass('moderation-preview-processed');
-};
-
-Drupal.behaviors.moderationPreview = function(context) {
-  Drupal.moderationPreview(context);
-};
-
-Drupal.moderationButton = function() {
-  var id = $(this).attr("id");
-  var type = id.split('-')[1]
-  var obj_id = id.split('-')[3];
-  var url = window.location.protocol+'//'+window.location.hostname+(window.location.port ? ':'+window.location.port : '')+Drupal.settings.basePath+$(this).attr("href");
-  var text = {
-    'status': {0: Drupal.t('publish'), 1: Drupal.t('unpublish')},
-    'promote': {0: Drupal.t('promote'), 1: Drupal.t('demote')},
-    'sticky': {0: Drupal.t('make sticky'), 1: Drupal.t('remove stickiness')},
-    'moderate': {0: Drupal.t('moderate'), 1: Drupal.t('unmoderate')}
+  Drupal.behaviors.moderationPreview = function(context) {
+    Drupal.moderationPreview(context);
+    Drupal.moderationButtons(context);
   };
 
-  $(this).click(function () {
-    var link = $(this);
-    link.addClass('throbbing');
+  Drupal.moderationPreview = function(context) {
+    var base_url = window.location.protocol+'//'+window.location.hostname+(window.location.port ? ':'+window.location.port : '')+Drupal.settings.basePath;
 
-    //load node data
-    var html = $.ajax({
-      url: url+'&js=1',
-      dataType: "json",
-      success: function(result){
-        // Exception for comments status
-        if (result[0] && !(result[3] == 'comment' && type == 'status')) {
-          if (result[1]) $('#'+id).html(text[type][1]);
-          else $('#'+id).html(text[type][0]);
+    $("a.moderation-ajax-preview:not('.moderation-processed')", context).
+    addClass('moderation-processed').
+    parent().parent().parent().each(function(index) {
+      var wrapper = $(this);
+      wrapper.append('<div class="moderation-preview"></div>');
+      var obj_id = $('div.moderation-preview-attributes', wrapper).attr('entity-id');
+      var type = $('div.moderation-preview-attributes', wrapper).attr('type');
+      var preview = $('.moderation-preview', wrapper);
+      var url = base_url+"?q=moderation/" + type + "/" + obj_id + "/get/preview";
+
+      $('a.moderation-ajax-preview', wrapper).click(function(event) {
+        event.preventDefault();
+        var link = $(this);
+
+        if (preview.html().length == 0) {
+          link.addClass('throbbing');
+
+          // load preview data
+          var html = $.ajax({
+            url: url+'&js=1',
+            dataType: "json",
+            success: function(html) {
+              preview.append(html);
+              Drupal.attachBehaviors(preview);
+              link.removeClass('throbbing');
+              preview.show();
+            }
+          });
         }
-        else {
-          if (result[1]) $('#'+id).html(text[type][0]);
-          else $('#'+id).html(text[type][1]);
-        }
-        if (result[1] && type == 'moderate') {
-          $('#'+id).parent().parent().parent().next('.moderation-preview').hide('slow');
-        }
-        link.removeClass('throbbing');
-      }
+
+        preview.toggle();
+
+        $.ajax({
+          url: base_url+"?q=moderation/" + type + "/"+obj_id+"/get/moderate&js=1",
+          dataType: "json",
+          success: function(status) {
+            preview.find('.moderation-messages').remove();
+            if (status[0] == 1) {
+              preview.prepend('<div class="moderation-messages status">'+Drupal.t('This item has already been moderated.')+'</div>');
+            }
+          }
+        });
+
+      })
     });
-    return false;
-  });
-};
+  };
 
-Drupal.behaviors.moderationInit = function (context) {
-  $('.moderation-content', context).each(Drupal.moderationPreview);
-  $('.moderation-status-link', context).each(Drupal.moderationButton);
-  $('.moderation-promote-link', context).each(Drupal.moderationButton);
-  $('.moderation-sticky-link', context).each(Drupal.moderationButton);
-  $('.moderation-moderate-link', context).each(Drupal.moderationButton);
-};
+  Drupal.moderationButtons = function(context) {
+    $("a.moderation-status-link, a.moderation-promote-link, a.moderation-sticky-link, a.moderation-moderate-link", context).
+    addClass('moderation-processed').
+    each(function(index) {
+      var id = $(this).attr("id");
+      var type = id.split('-')[1]
+      var obj_id = id.split('-')[3];
+      var url = window.location.protocol+'//'+window.location.hostname+(window.location.port ? ':'+window.location.port : '')+Drupal.settings.basePath+$(this).attr("href");
+      var text = {
+        'status': {
+          0: Drupal.t('publish'),
+          1: Drupal.t('unpublish')
+        },
+        'promote': {
+          0: Drupal.t('promote'),
+          1: Drupal.t('demote')
+        },
+        'sticky': {
+          0: Drupal.t('make sticky'),
+          1: Drupal.t('remove stickiness')
+        },
+        'moderate': {
+          0: Drupal.t('moderate'),
+          1: Drupal.t('unmoderate')
+        }
+      };
 
+      $(this).click(function (event) {
+        event.preventDefault();
+        var link = $(this);
+        link.addClass('throbbing');
+
+        //load node data
+        var html = $.ajax({
+          url: url+'&js=1',
+          dataType: "json",
+          success: function(result) {
+            // Exception for comments status
+            if (result[0] && !(result[3] == 'comment' && type == 'status')) {
+              if (result[1]) $('#'+id).html(text[type][1]);
+              else $('#'+id).html(text[type][0]);
+            }
+            else {
+              if (result[1]) $('#'+id).html(text[type][0]);
+              else $('#'+id).html(text[type][1]);
+            }
+            if (result[1] && type == 'moderate') {
+              $('#'+id).parent().parent().parent().parent().parent().find('.moderation-preview').hide('slow');
+            }
+            link.removeClass('throbbing');
+          }
+        });
+      });
+    });
+  };
 })(jQuery);
